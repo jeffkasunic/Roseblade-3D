58183caa363df8dcfdf277c35dbe87cdc69712ce
Jeff Kasunic <github.kwb8u@dev.jeffkasunic.com>
2026-06-21 12:28:41 -0700

    Hello window

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,53 @@
 #include <GLFW/glfw3.h>
 #include <iostream>
 
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+
+// Settints
+const unsigned int SCR_WIDTH = 800;
+const unsigned int SCR_HEIGHT = 600;
+
 int main() {
-    std::cout << "Hello, friend." << std::endl;
+    glfwInit();
+    /*
+     * Apple deprecated OpenGL in macOS 10.14 (Mojave) in favor of Metal. The OpenGL version on macOS is capped at OpenGL 4.1 (from 2010) regardless
+     * of your GPU's capabilities, and it has not been updated since. So if you're writing OpenGL code on macOS, 4.1 is the ceiling you should target.
+     */
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 1);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+    
+    GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "Roseblade 3D", nullptr, nullptr);
+    if (!window) {
+        std::cout << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+    
+    // Initialize GLAD
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+        std::cout << "Failed to initialize GLAD" << std::endl;
+        return -1;
+    }
+    
+    /*
+     * Create the render loop.
+     * We don't want the application to draw a single image and then immediately quit and close the window.
+     * We want the application to keep drawing images and handling user input until the program has been explicitly told to stop. 
+     */
+    while (!glfwWindowShouldClose(window)) {
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+    
+    glfwTerminate();
+    return 0;
+}
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
+    glViewport(0, 0, width, height);
 }
