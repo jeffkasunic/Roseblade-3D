c0a5908de9645a9ed080aeada6fb5b39604bb7fd
Jeff Kasunic <github.kwb8u@dev.jeffkasunic.com>
2026-06-21 17:15:50 -0700

    Hello window

--- src/main.cpp
+++ src/main.cpp
@@ -4,43 +4,57 @@
 
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+void processInput(GLFWwindow* window);
 
-// Settints
+// Settings
 const unsigned int SCR_WIDTH = 800;
 const unsigned int SCR_HEIGHT = 600;
 
 int main() {
-    glfwInit();
-    /*
+    /* Initialize and configure glfw
+     * -----------------------------
      * Apple deprecated OpenGL in macOS 10.14 (Mojave) in favor of Metal. The OpenGL version on macOS is capped at OpenGL 4.1 (from 2010) regardless
-     * of your GPU's capabilities, and it has not been updated since. So if you're writing OpenGL code on macOS, 4.1 is the ceiling you should target.
+     * of GPU's capabilities, and it has not been updated since. So on macOS, 4.1 is the ceiling you should target.
      */
+    glfwInit();
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 1);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
     glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
     
+    // glfw window creation
     GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "Roseblade 3D", nullptr, nullptr);
     if (!window) {
-        std::cout << "Failed to create GLFW window" << std::endl;
+        std::cerr << "Failed to create GLFW window" << std::endl;
         glfwTerminate();
         return -1;
     }
     glfwMakeContextCurrent(window);
     glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
     
-    // Initialize GLAD
+    // glad: load all OpenGL function pointers
+    // ---------------------------------------
     if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
-        std::cout << "Failed to initialize GLAD" << std::endl;
+        std::cerr << "Failed to initialize GLAD" << std::endl;
         return -1;
     }
     
     /*
-     * Create the render loop.
+     * Render loop
+     * -----------
      * We don't want the application to draw a single image and then immediately quit and close the window.
      * We want the application to keep drawing images and handling user input until the program has been explicitly told to stop. 
      */
     while (!glfwWindowShouldClose(window)) {
+        // Input
+        processInput(window);
+        
+        // Render
+        glClearColor(0.18f, 0.18f, 0.18f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+        
+        // glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
+        // -------------------------------------------------------------------------------
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
@@ -51,4 +65,10 @@ int main() {
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
     glViewport(0, 0, width, height);
+}
+
+void processInput(GLFWwindow* window) {
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
+        glfwSetWindowShouldClose(window, true);
+    }
 }
